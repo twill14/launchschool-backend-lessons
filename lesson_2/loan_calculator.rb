2bb74a5f21f0d6b95836ed2a3394d81b68b10db0
Thomas Williams <twill14@gmail.com>
2016-08-27 12:37:26 -0500

    Made adjustments to loan_calculator after rubocop run

--- lesson_2/loan_calculator.rb
+++ lesson_2/loan_calculator.rb
@@ -15,13 +15,12 @@ def validate_percent?(num)
 end
 
 def monthly_interest_rate(apr)
-   apr /= 100
-   apr /= 12
+  apr /= 100
+  apr / 12
 end
 
 def calculation_method(la, mir, ldm)
-  result = la * (mir / (1 - (1 + mir)**-ldm))
-  return result
+  la * (mir / (1 - (1 + mir)**-ldm))
 end
 
 def loan_duration_month(dur)
@@ -32,37 +31,37 @@ system_prompt(SYSTEM['welcome'])
 
 loop do
   loan_amount = ''
-    loop do
-      system_prompt(SYSTEM['loan_amount'])
-      loan_amount = gets.chomp.to_i
-      if validate_number? loan_amount
-        break
-      else
-        system_prompt(SYSTEM['invalid_number'])
-      end
+  loop do
+    system_prompt(SYSTEM['loan_amount'])
+    loan_amount = gets.chomp.to_i
+    if validate_number? loan_amount
+      break
+    else
+      system_prompt(SYSTEM['invalid_number'])
     end
+  end
 
   loan_apr = ''
-    loop do
-      system_prompt(SYSTEM['loan_apr'])
-      loan_apr = gets.chomp.to_f
-      if validate_percent? loan_apr
-        break
-      else
-        system_prompt(SYSTEM['loan_apr_invalid'])
-      end
+  loop do
+    system_prompt(SYSTEM['loan_apr'])
+    loan_apr = gets.chomp.to_f
+    if validate_percent? loan_apr
+      break
+    else
+      system_prompt(SYSTEM['loan_apr_invalid'])
     end
+  end
 
-    loan_duration = ''
-    loop do
-      system_prompt(SYSTEM['loan_duration'])
-      loan_duration = gets.to_i
-      if validate_number? loan_duration
-        break
-      else
-        system_prompt(SYSTEM['invalid_number'])
-      end
+  loan_duration = ''
+  loop do
+    system_prompt(SYSTEM['loan_duration'])
+    loan_duration = gets.chomp.to_i
+    if validate_number? loan_duration
+      break
+    else
+      system_prompt(SYSTEM['invalid_number'])
     end
+  end
 
   mir = monthly_interest_rate(loan_apr)
   dur = loan_duration_month(loan_duration)
